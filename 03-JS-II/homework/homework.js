5cf057409bb802d7636858e3e4f517a0f619b047
J001NT <basster_18@yahoo.com.ar>
2022-05-30 11:16:02 -0300

    Tabajando en homework 03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,16 +1,22 @@
 // No cambies los nombres de las funciones.
 
 function obtenerMayor(x, y) {
+  
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) return (x);
+    else if (y > x) return (y);
+    else if (x === y) return (x||y);
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) return ("Allowed");
+  return ("Not allowed")
 }
   
 function conection(status) {
@@ -19,6 +25,9 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) return ("Online");
+  else if (status === 2) return ("Away");
+  return ("Offline")
 }
 
 function saludo(idioma) {
